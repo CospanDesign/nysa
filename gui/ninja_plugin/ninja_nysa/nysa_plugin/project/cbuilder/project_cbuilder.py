fff0fed78744a68b0c365815c5f0de8985cc38fa
Dave McCoy <cospan@gmail.com>
2013-04-16 05:25:46 -0400

    Added a second page to the new project wizard
    This page is different depending on whether the user selected
    Axi or Wishbone as the bus structure

--- gui/ninja_plugin/ninja_nysa/nysa_plugin/project/cbuilder/project_cbuilder.py
+++ gui/ninja_plugin/ninja_nysa/nysa_plugin/project/cbuilder/project_cbuilder.py
@@ -2,10 +2,7 @@
 
 import os
 import sys
-#try:
-#  import json
-#except ImportError:
-#  import simplejson as json
+import json
 
 from PyQt4.QtGui import QMessageBox
 #from PyQt4.QtCore import Qt
@@ -15,8 +12,9 @@ from ninja_ide.core import plugin_interfaces
 from ninja_ide.core import file_manager
 from ninja_ide.tools import json_manager
 
-from menu import Menu
-from wizard import PagePluginProperties
+from .menu import Menu
+from .wizard import PagePluginProperties
+from .wizard import CoreCustomize
 
 PROJECT_TYPE = "Verilog Core Builder"
 
@@ -25,68 +23,71 @@ sys.path.append(os.path.join(os.path.dirname(__file__), os.pardir, os.pardir))
 
 class ProjectCbuilder(plugin_interfaces.IProjectTypeHandler):
 
-  EXT = 'cbldr'
-  output = None
+    EXT = 'cbldr'
+    output = None
 
-  def __init__(self, output = None, locator = None):
-      self.locator = locator
-      self.output = output
+    def __init__(self, output=None, locator=None):
+        self.locator = locator
+        self.output = output
 
-  def get_context_menus(self):
-    return (Menu(self.locator, self.output), )
+    def get_context_menus(self):
+        return (Menu(self.locator, self.output), )
 
-  def get_pages(self):
-    return [PagePluginProperties(self.locator)]
+    def get_pages(self):
+        pages = []
+        ppp = PagePluginProperties(self.locator, self.output)
+        cc = CoreCustomize(self.locator, self.output)
+        ppp.set_slave_type_func(cc.set_slave_type)
+        pages.append(ppp)
+        pages.append(cc)
+        return pages
 
-  def on_wizard_finish(self, wizard):
-    global PROJECT_TYPE
-    ids = wizard.pageIds()
-    self.output.Debug(self, "ids: %s" % str(ids))
-    page = wizard.page(ids[2])
-    path = unicode(page.txtPlace.text())
-    if not path:
-      QMessageBox.critical(self, self.tr("Incorrect Location"),
-        self.tr("The project couldn\'t be created"))
-      return
-    name = unicode(page.txtName.text())
-    project = {}
-    project['name'] = name
-    project['project-type'] = PROJECT_TYPE
-    project['description'] = unicode(page.txtDescription.toPlainText())
-    project['license'] = unicode(page.cboLicense.currentText())
-    project['venv'] = unicode(page.vtxtPlace.text())
+    def on_wizard_finish(self, wizard):
+        global PROJECT_TYPE
+        ids = wizard.pageIds()
+        page = wizard.page(ids[2])
+        path = str(page.txtPlace.text())
+        if not path:
+            QMessageBox.critical(self, self.tr("Incorrect Location"),
+                self.tr("The project couldn\'t be created"))
+            return
+        name = str(page.txtName.text())
+        project = {}
+        project['name'] = str(name)
+        project['project-type'] = str(PROJECT_TYPE)
+        project['description'] = str(page.txtDescription.toPlainText())
+        project['license'] = str(page.cboLicense.currentText())
+        project['venv'] = str(page.vtxtPlace.text())
 
+        path = os.path.join(path, name)
 
+        file_manager.create_folder(path, add_init_file=False)
+        json_manager.create_ninja_project(path, name, project)
 
-    path = os.path.join(path, name)
+        page = wizard.page(ids[1])
+        #plugin_dict = self.create_descriptor(page, path)
+        self.plugin_dict = self.create_descriptor(page, path)
+        #self.create_plugin_class(page, path, plugin_dict)
+        wizard._load_project(path)
 
-    file_manager.create_folder(path, add_init_file=False)
-    json_manager.create_ninja_project(path, name, project)
+    def create_descriptor(self, page, path):
+        plugin = {}
 
-    plugin_dict = self.create_descriptor(page, path)
-    self.create_plugin_class(page, path, plugin_dict)
-    wizard._load_project(path)
+        core_name = str(page.txtCore.text())
+        plugin['core'] = core_name
+        authors = str(page.txtAuthors.text())
+        plugin['authors'] = authors
+        version = str(page.txtVersion.text())
+        plugin['version'] = version
 
+        fileName = os.path.join(path, core_name + "." + self.EXT)
+        # Create the .plugin file with metadata
+        self.create_file(fileName, plugin)
+        # Return the dictionary
+        return plugin
 
-
-  def create_descriptor(self, page, path):
-    plugin = {}
-
-    #module = unicode(page.txtModule.text())
-    #plugin['module'] = module
-    #className = str(page.txtClass.text())
-    #plugin['class'] = className
-    authors = unicode(page.txtAuthors.text())
-    plugin['authors'] = authors
-    url = unicode(page.txtUrl.text())
-    plugin['url'] = url
-    version = unicode(page.txtVersion.text())
-    plugin['version'] = version
-
-    fileName = os.path.join(path, module + self.EXT)
-    # Create the .plugin file with metadata
-    self.create_file(fileName, plugin)
-    # Return the dictionary
-    return plugin
-
-
+    def create_file(self, fileName, structure):
+        self.output.Info(self, "Creating file: %s" % fileName)
+        f = open(fileName, mode='w')
+        json.dump(structure, f, indent=2)
+        f.close()
--- gui/ninja_plugin/ninja_nysa/nysa_plugin/project/cbuilder/wizard.py
+++ gui/ninja_plugin/ninja_nysa/nysa_plugin/project/cbuilder/wizard.py
@@ -1,27 +1,37 @@
 # -*- coding: utf-8 *-*
 
-import re
+#import re
 
 from PyQt4.QtGui import QWizardPage
 from PyQt4.QtGui import QGridLayout
 from PyQt4.QtGui import QLabel
 from PyQt4.QtGui import QLineEdit
-#from PyQt4.QtGui import QCheckBox
+from PyQt4.QtGui import QCheckBox
 from PyQt4.QtGui import QRadioButton
-from PyQt4.QtGui import QMessageBox
+#from PyQt4.QtGui import QMessageBox
+
+
+def enum(*sequential, **named):
+    enums = dict(zip(sequential, range(len(sequential))), **named)
+    return type('Enum', (), enums)
+
+
+SLAVE_TYPE = enum('WISHBONE', 'AXI')
+
 
 class PagePluginProperties(QWizardPage):
 
-    def __init__(self, locator):
+    def __init__(self, locator, output):
         QWizardPage.__init__(self)
         # service locator
         self.locator = locator
+        self.output = output
         # grid
         grid = QGridLayout(self)
         grid.addWidget(QLabel('Core Name:'), 0, 0)
-        self.txtModule = QLineEdit()
-        grid.addWidget(self.txtModule, 0, 1)
-        self.registerField('moduleName*', self.txtModule)
+        self.txtCore = QLineEdit()
+        grid.addWidget(self.txtCore, 0, 1)
+        self.registerField('coreName*', self.txtCore)
 
         grid.addWidget(QLabel("Bus Type"), 1, 0)
         self.axi_radio = QRadioButton("Axi")
@@ -32,50 +42,75 @@ class PagePluginProperties(QWizardPage):
         #grid.addWidget(QLabel('Axi'), 2, 1)
         grid.addWidget(self.axi_radio, 2, 1)
 
-        grid.addWidget(QLabel('Author(s):'), 2, 0)
+        grid.addWidget(QLabel('Author(s):'), 3, 0)
         self.txtAuthors = QLineEdit()
-        grid.addWidget(self.txtAuthors, 2, 1)
+        grid.addWidget(self.txtAuthors, 3, 1)
         self.registerField('txtAuthors*', self.txtAuthors)
 
-        grid.addWidget(QLabel('Url:'), 3, 0)
-        self.txtUrl = QLineEdit()
-        grid.addWidget(self.txtUrl, 3, 1)
+        #grid.addWidget(QLabel('Url:'), 4, 0)
+        #self.txtUrl = QLineEdit()
+        #grid.addWidget(self.txtUrl, 4, 1)
 
-        grid.addWidget(QLabel('Version:'), 4, 0)
+        grid.addWidget(QLabel('Version:'), 5, 0)
         self.txtVersion = QLineEdit("0.1")
-        grid.addWidget(self.txtVersion, 4, 1)
+        grid.addWidget(self.txtVersion, 5, 1)
+        self.func = None
 
-        '''
-        # Plugin services
-        grid.addWidget(QLabel('Services:'), 5, 0)
+        #Add a second page that is specific to either Axi or wishbone
 
-        self.checkEditorS = QCheckBox('Editor')
-        grid.addWidget(self.checkEditorS, 6, 0)
+        #Axi/Wishbone
+        #   Add the capability to be a master (arbitrator) Axi/Wisbhone
 
-        self.checkToolbarS = QCheckBox('Toolbar')
-        grid.addWidget(self.checkToolbarS, 6, 1)
-
-        self.checkMenuPluginS = QCheckBox('Menu Plugin')
-        grid.addWidget(self.checkMenuPluginS, 6, 2)
-
-        self.checkMiscS = QCheckBox('Misc Container')
-        grid.addWidget(self.checkMiscS, 7, 0)
-
-        self.checkExplorerS = QCheckBox('Explorer')
-        grid.addWidget(self.checkExplorerS, 7, 1)
-        '''
+        #Axi Page:
+        #   Add the option to be a simple Axi extension
+    def set_slave_type_func(self, func):
+        self.func = func
 
     def validatePage(self):
-        pat_module_name = re.compile("^[a-z_]+$")
-        #pat_class_name = re.compile(r"([a-zA-Z_]+[0-9]*)+$")
-        if not pat_module_name.match(self.txtModule.text()):
-            QMessageBox.information(self, 'Validation error',
-                'The module name is invalid')
-            return False
+        if self.axi_radio.isChecked():
+            self.func(SLAVE_TYPE.AXI)
+        else:
+            self.func(SLAVE_TYPE.WISHBONE)
+
         """
+        pat_core_name = re.compile("^[a-z_]+$")
+        pat_class_name = re.compile(r"([a-zA-Z_]+[0-9]*)+$")
+        if not pat_core_name.match(self.txtModule.text()):
+            QMessageBox.information(self, 'Validation error',
+                'The core name is invalid')
+            return False
         if not pat_class_name.match(self.txtClass.text()):
             QMessageBox.information(self, 'Validation error',
                 'The class name is invalid')
             return False
         """
+        return True
+
+
+class CoreCustomize(QWizardPage):
+    def __init__(self, locator, output):
+        QWizardPage.__init__(self)
+        self.locator = locator
+        self.output = output
+        self.grid = QGridLayout(self)
+        self.slave_type = SLAVE_TYPE.WISHBONE
+        self.cb_wb_memory_master = None
+        self.rb_axi_simple = None
+        self.rb_axi_normal = None
+
+    def set_slave_type(self, slave_type):
+        if slave_type is SLAVE_TYPE.AXI:
+            self.output.Info(self, "AXI Selected")
+            self.rb_axi_simple = QRadioButton("Simple Slave")
+            self.rb_axi_normal = QRadioButton("Normal Slave")
+            self.rb_axi_simple.setChecked(True)
+            self.grid.addWidget(self.rb_axi_simple, 1, 0)
+            self.grid.addWidget(self.rb_axi_normal, 2, 0)
+
+        elif slave_type is SLAVE_TYPE.WISHBONE:
+            self.output.Info(self, "Wishbone Selected")
+            self.cb_wb_memory_master = QCheckBox('Master')
+            self.grid.addWidget(self.cb_wb_memory_master, 1, 0)
+
+    def validate_page(self):
         return True
