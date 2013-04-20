b0e02954bdb14202c5fb05b787445b76622c0cdb
Dave McCoy <cospan@gmail.com>
2013-04-19 22:11:07 -0400

    Started working on the preference plugin

--- gui/ninja_plugin/ninja_nysa/nysa_plugin/preferences/preferences.py
+++ gui/ninja_plugin/ninja_nysa/nysa_plugin/preferences/preferences.py
@@ -1,1 +1,35 @@
 # -*- coding: utf-8 *-*
+import os
+
+from PyQt4.QtGui import QWidget
+from PyQt4.QtGui import QGridLayout
+from PyQt4.QtGui import QLabel
+from PyQt4.QtGui import QLineEdit
+
+
+class NysaPreferences(QWidget):
+
+    pdict = {}
+  
+    def __init__(self, locator, output):
+        QWidget.__init__(self)
+        self.locator = locator
+        self.output = output
+        self.output.Debug(self, "Creating preference widget")
+        pdict = {}
+        user_dir = os.path.expanduser("~")
+        pdict["CBuilder_location"] = os.path.join(user_dir,
+          "/Projects/cbuilder_projects")
+
+        self.init_ui()
+
+    def init_ui(self):
+        self.setWindowTitle('Preference')
+        grid = QGridLayout(self)
+        grid.addWidget(QLabel('Core Builder Base Directory:'), 0, 0)
+        self.txtCBuilderLocation = QLineEdit(pdict["CBuilder_location"])
+        grid.addWidget(self.txtCBuilderLocation, 1, 0)
+
+    def get_preference_dict(self):
+      return self.pdict
+    
