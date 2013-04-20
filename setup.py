d16e3c8a2d7fbe244f250dfd329b7c03d35aa01b
Dave McCoy <cospan@gmail.com>
2013-04-19 22:09:46 -0400

    Add a setup file to setup nysa

---
+++ setup.py
@@ -0,0 +1,11 @@
+#! /usr/bin/env python
+
+from distutils.core import setup
+
+setup(  name='Nysa',
+        version='0.0.1',
+        description='FPGA core/image generator and FPGA communication',
+        author='Dave McCoy',
+        author_email='dave.mccoy@cospandesign.com',
+        packages=['']
+)
