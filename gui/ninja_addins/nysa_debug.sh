6a7c6d68fd8e98c295ba6e3eafa06c6f82c1825e
Dave McCoy <cospan@gmail.com>
2013-05-11 13:45:42 -0400

    Aded files that are used with the ninja-ide

---
+++ gui/ninja_addins/nysa_debug.sh
@@ -0,0 +1,4 @@
+#! /bin/bash
+
+/home/cospan/Projects/ninja-ide/ninja-ide.py --loglevel DEBUG --logfile /home/cospan/sandbox/ninja_ide_log.txt --plugin /home/cospan/Projects/nysa/gui/ninja_plugin/ninja_nysa
+
