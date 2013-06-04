0de3b6e9d905e0de8ebe2e98e2e12a50d85844f2
Dave McCoy <cospan@gmail.com>
2013-06-04 18:51:36 -0400

    Added description and log

--- test/tester.py
+++ test/tester.py
@@ -1,5 +1,42 @@
 #! /usr/bin/python
 
+#Distributed under the MIT licesnse.
+#Copyright (c) 2011 Dave McCoy (dave.mccoy@cospandesign.com)
+
+#Permission is hereby granted, free of charge, to any person obtaining a copy of
+#this software and associated documentation files (the "Software"), to deal in
+#the Software without restriction, including without limitation the rights to
+#use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+#of the Software, and to permit persons to whom the Software is furnished to do
+#so, subject to the following conditions:
+#
+#The above copyright notice and this permission notice shall be included in all
+#copies or substantial portions of the Software.
+#
+#THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+#IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+#FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+#AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+#LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+#OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+#SOFTWARE.
+
+"""Unit test controller
+
+Description: A command line unit test controller, put this in the directory
+of python unit test, the names of the files need to start with 'test_' and
+end with '.py' it will do the rest
+
+"""
+
+"""
+Log:
+
+5/27/2013: Initial commit, many of this code was taken from the Olympus 
+  'run_test.py' script
+"""
+
+
 import sys
 import os
 import argparse
