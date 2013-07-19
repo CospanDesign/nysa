0d0fb4594ac835cf34dfbfaad1570bbcef088340
cospan <cospan@gmail.com>
2013-07-19 19:26:31 -0400

    fixed a missing signal on ping pong FIFO

--- cbuilder/verilog/generic/ppfifo.v
+++ cbuilder/verilog/generic/ppfifo.v
@@ -3,21 +3,21 @@ Distributed under the MIT licesnse.
 Copyright (c) 2011 Dave McCoy (dave.mccoy@cospandesign.com)
 
 Permission is hereby granted, free of charge, to any person obtaining a copy of
-this software and associated documentation files (the "Software"), to deal in 
-the Software without restriction, including without limitation the rights to 
-use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies 
-of the Software, and to permit persons to whom the Software is furnished to do 
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+of the Software, and to permit persons to whom the Software is furnished to do
 so, subject to the following conditions:
 
-The above copyright notice and this permission notice shall be included in all 
+The above copyright notice and this permission notice shall be included in all
 copies or substantial portions of the Software.
 
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, 
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE 
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 */
 
@@ -41,7 +41,6 @@ module ppfifo
   write_strobe,
   write_data,
 
-  starved,
 
   //read
   read_clock,
@@ -49,7 +48,11 @@ module ppfifo
   read_ready,
   read_activate,
   read_count,
-  read_data
+  read_data,
+
+  //general status
+  starved,
+  inactive
 );
 
 parameter FIFO_DEPTH = (1 << ADDRESS_WIDTH);
@@ -73,7 +76,7 @@ input                       read_activate;
 output reg [23:0]           read_count;
 output [DATA_WIDTH - 1: 0]  read_data;
 
-
+output                      inactive;
 
 
 //Local Registers/Wires
@@ -103,9 +106,19 @@ reg   [DATA_WIDTH - 1: 0]   last_read_data;
 reg                         pre_read_strobe0;
 reg                         pre_read_strobe1;
 
-reg                         first_read0;
-reg                         first_read1;
+//Cross Clock Reset
+reg                         r_reset;
+reg                         w_reset;
+wire                        both_reset;
+reg                         r_reset_ack;
+reg                         w_reset_ack;
 
+reg   [1:0]                 waiting;
+
+reg   [1:0]                 read_pre_select;
+
+wire                        read_pre_select0;
+wire                        read_pre_select1;
 
 //Cross clock status
 reg   [1:0]                 fifo0_ready_history;
@@ -120,11 +133,6 @@ reg   [2:0]                 write_clock_pulse_empty0_valid_sync;
 wire                        write_clock_pulse_empty1_valid;
 reg   [2:0]                 write_clock_pulse_empty1_valid_sync;
 
-reg   [1:0]                 read_pre_select;
-
-wire                        read_pre_select0;
-wire                        read_pre_select1;
-
 wire                        pos_edge_fifo0_ready;
 wire                        pos_edge_fifo1_ready;
 
@@ -133,13 +141,13 @@ reg                         prev_fifo1_ready;
 
 
 
-afifo 
+afifo
   #(
   .DATA_WIDTH(DATA_WIDTH),
   .ADDRESS_WIDTH(ADDRESS_WIDTH)
   )
   fifo0(
-    .rst(reset),
+    .rst(r_reset | w_reset),
 
     //read
     .dout_clk(read_clock),
@@ -159,7 +167,7 @@ afifo #(
   .ADDRESS_WIDTH(ADDRESS_WIDTH)
   )
   fifo1(
-    .rst(reset),
+    .rst(r_reset | w_reset),
 
     //read
     .dout_clk(read_clock),
@@ -175,36 +183,101 @@ afifo #(
 );
 
 //asynchronous logic
-assign  write_fifo_size = FIFO_DEPTH;
+assign  write_fifo_size                 = FIFO_DEPTH;
 
-assign fifo0_write_data     = (write_activate[0]) ? write_data : 0; 
-assign fifo1_write_data     = (write_activate[1]) ? write_data : 0;
+assign fifo0_write_data                 = (write_activate[0]) ? write_data : 0;
+assign fifo1_write_data                 = (write_activate[1]) ? write_data : 0;
 
-assign fifo0_write_strobe   = (write_activate[0]) ? write_strobe : 0;
-assign fifo1_write_strobe   = (write_activate[1]) ? write_strobe : 0;
+assign fifo0_write_strobe               = (write_activate[0]) ? write_strobe : 0;
+assign fifo1_write_strobe               = (write_activate[1]) ? write_strobe : 0;
 
-assign write_ready          = {fifo1_empty && (fifo1_write_count == 0) , fifo0_empty && (fifo0_write_count == 0)};
+assign write_ready                      = (w_reset | r_reset) ? 2'b00 :
+                                          {fifo1_empty && (fifo1_write_count == 0) , fifo0_empty && (fifo0_write_count == 0)};
 
-assign starved              = ((write_activate[0] == 1) && fifo1_empty) || 
-                              ((write_activate[1] == 1) && fifo0_empty) ||
-                              (fifo0_empty && fifo1_empty);
+assign starved                          = ((write_activate[0] == 1) && fifo1_empty) ||
+                                          ((write_activate[1] == 1) && fifo0_empty) ||
+                                          (fifo0_empty && fifo1_empty);
+
 
 //assign the read output
-assign read_data            = (read_fifo_select[0] == 1) ? fifo0_read_data :
-                              (read_fifo_select[1] == 1) ? fifo1_read_data :
-                              last_read_data;
+assign read_data                        = (read_fifo_select[0] == 1) ? fifo0_read_data :
+                                          (read_fifo_select[1] == 1) ? fifo1_read_data :
+                                          last_read_data;
 
-assign fifo0_read_strobe    = ((read_fifo_select[0] == 1) ? read_strobe : 0) | pre_read_strobe0;
-assign fifo1_read_strobe    = ((read_fifo_select[1] == 1) ? read_strobe : 0) | pre_read_strobe1;
+assign fifo0_read_strobe                = ((read_fifo_select[0] == 1) ? read_strobe : 0) | pre_read_strobe0;
+assign fifo1_read_strobe                = ((read_fifo_select[1] == 1) ? read_strobe : 0) | pre_read_strobe1;
 
+assign both_reset                       = r_reset & w_reset;
+
+
+assign inactive                            = fifo0_empty &&
+                                          fifo1_empty &&
+                                          (write_activate == 0) &&
+                                          !read_activate &&
+                                          !fifo0_ready &&
+                                          !fifo1_ready;
+
+//Cross Clock Domain Signals
 assign  write_clock_pulse_empty0_valid  = (~write_clock_pulse_empty0_valid_sync[2] && write_clock_pulse_empty0_valid_sync[1]);
 assign  write_clock_pulse_empty1_valid  = (~write_clock_pulse_empty1_valid_sync[2] && write_clock_pulse_empty1_valid_sync[1]);
 
 
-//debug signals so the simulator can view it
-assign read_pre_select0     = read_pre_select[0];
-assign read_pre_select1     = read_pre_select[1];
+//Read side synchronization of write side status
+assign  fifo0_ready = (fifo0_ready_history[1] & fifo0_ready_history[0]);
+assign  fifo1_ready = (fifo1_ready_history[1] & fifo1_ready_history[0]);
+
+assign  pos_edge_fifo0_ready  = fifo0_ready & ~prev_fifo0_ready;
+assign  pos_edge_fifo1_ready  = fifo1_ready & ~prev_fifo1_ready;
+
+
+
+
+
 //synchronous logic
+
+//Cross Clock Domain Reset
+always @(posedge read_clock or posedge reset) begin
+  if (reset) begin
+    r_reset             <=  1;
+    r_reset_ack         <=  0;
+  end
+  else begin
+    if (w_reset && !both_reset && !r_reset_ack) begin
+      r_reset           <=  1;
+      r_reset_ack       <=  0;
+    end
+    if (both_reset) begin
+      r_reset_ack       <=  1;
+    end
+    if (r_reset &&  w_reset_ack) begin
+      r_reset           <=  0;
+    end
+    if (!r_reset && !w_reset) begin
+      r_reset_ack       <=  0;
+    end
+  end
+end
+always @(posedge write_clock or posedge reset) begin
+  if (reset) begin
+    w_reset             <=  1;
+    w_reset_ack         <=  0;
+  end
+  else begin
+    if (r_reset && !both_reset && !w_reset_ack) begin
+      w_reset           <=  1;
+      w_reset_ack       <=  0;
+    end
+    if (both_reset) begin
+      w_reset_ack       <=  1;
+    end
+    if (w_reset && r_reset_ack) begin
+      w_reset           <=  0;
+    end
+    if (!w_reset && !r_reset) begin
+      w_reset_ack       <=  0;
+    end
+  end
+end
 
 always @(posedge write_clock or posedge reset) begin
   if (reset) begin
@@ -212,8 +285,12 @@ always @(posedge write_clock or posedge reset) begin
     write_clock_pulse_empty1_valid_sync <=  0;
   end
   else begin
-    write_clock_pulse_empty0_valid_sync <=  {write_clock_pulse_empty0_valid_sync[1:0], fifo0_empty};
-    write_clock_pulse_empty1_valid_sync <=  {write_clock_pulse_empty1_valid_sync[1:0], fifo1_empty};
+    if (!waiting[0]) begin
+      write_clock_pulse_empty0_valid_sync <=  {write_clock_pulse_empty0_valid_sync[1:0], fifo0_empty};
+    end
+    if (!waiting[1]) begin
+      write_clock_pulse_empty1_valid_sync <=  {write_clock_pulse_empty1_valid_sync[1:0], fifo1_empty};
+    end
   end
 end
 
@@ -248,20 +325,15 @@ always @ (posedge write_clock or posedge reset) begin
 end
 
 
-//Read side synchronization of write side status
-assign  fifo0_ready = (fifo0_ready_history[1] & fifo0_ready_history[0]);
-assign  fifo1_ready = (fifo1_ready_history[1] & fifo1_ready_history[0]);
-
-assign  pos_edge_fifo0_ready  = fifo0_ready & ~prev_fifo0_ready;
-assign  pos_edge_fifo1_ready  = fifo1_ready & ~prev_fifo1_ready;
-
 always @ (posedge read_clock or posedge reset) begin
   if (reset) begin
     fifo0_ready_history <=  0;
     fifo1_ready_history <=  0;
   end
   else begin
-    if (fifo0_empty) begin
+    //if (fifo0_empty && !single_read[0]) begin
+    if (fifo0_empty && !waiting[0]) begin
+    //if (fifo0_empty) begin
       fifo0_ready_history <=  0;
     end
     else begin
@@ -269,7 +341,9 @@ always @ (posedge read_clock or posedge reset) begin
       fifo0_ready_history[0] <= (!write_activate[0] && (fifo0_write_count != 0));
     end
 
-    if (fifo1_empty) begin
+    //if (fifo1_empty && !single_read[1]) begin
+    if (fifo1_empty && !waiting[1]) begin
+    //if (fifo1_empty) begin
       fifo1_ready_history <=  0;
     end
     else begin
@@ -293,13 +367,14 @@ always @ (posedge read_clock or posedge reset) begin
     pre_read_strobe0        <=  0;
     pre_read_strobe1        <=  0;
     read_pre_select         <=  0;
-    first_read0             <=  1;
-    first_read1             <=  1;
+//    single_read             <=  0;
+    waiting                 <=  0;
   end
   else begin
     pre_read_strobe0        <=  0;
     pre_read_strobe1        <=  0;
     if ((read_fifo_select == 0) && !read_activate) begin
+      //if there is data in the other FIFO allow that FIFO to have priority
       if (read_pre_select[0] && fifo0_ready) begin
           read_count          <=  fifo0_write_count;
           read_ready          <=  1;
@@ -307,23 +382,26 @@ always @ (posedge read_clock or posedge reset) begin
           read_pre_select     <=  0;
       end
       else if (read_pre_select[1] && fifo1_ready) begin
-          read_count          <=  fifo1_write_count; 
+          read_count          <=  fifo1_write_count;
           read_ready          <=  1;
           read_fifo_select[1] <=  1;
           read_pre_select     <=  0;
       end
       else begin
+        //read_pre_select     <=  0;
         if (fifo0_ready) begin
           //although this signal is asynchronous due to the synchronization of fifo0_redy I know that
           //the fifoX_write_count is stable
           read_count          <=  fifo0_write_count;
           read_ready          <=  1;
           read_fifo_select[0] <=  1;
+          read_pre_select     <=  0;
         end
         else if (fifo1_ready) begin
-          read_count          <=  fifo1_write_count; 
+          read_count          <=  fifo1_write_count;
           read_ready          <=  1;
           read_fifo_select[1] <=  1;
+          read_pre_select     <=  0;
         end
         else begin
           read_ready          <=  0;
@@ -332,46 +410,68 @@ always @ (posedge read_clock or posedge reset) begin
       end
     end
 
-    //if (pos_edge_fifo0_ready && first_read0) begin
+    //this takes care of the edge case when only one item goes into the PPFIFO
+    /*
+    if (read_fifo_select[0] && read_strobe) begin
+      single_read[0]          <=  0;
+    end
+    if (read_fifo_select[1] && read_strobe) begin
+      single_read[1]          <=  0;
+    end
+    */
     if (pos_edge_fifo0_ready) begin
+      waiting[0]              <=  1;
       pre_read_strobe0        <=  1;
-      first_read0             <=  0;
+/*
+      if (fifo0_write_count == 1) begin
+        single_read[0]          <=  1;
+      end
+*/
     end
-    //if (pos_edge_fifo1_ready && first_read1) begin
+
     if (pos_edge_fifo1_ready) begin
+      waiting[1]              <=  1;
       pre_read_strobe1        <=  1;
-      first_read1             <=  0;
+/*
+      if (fifo1_write_count == 1) begin
+        single_read[1]          <=  1;
+      end
+*/
     end
-    
+
     if (read_activate && read_ready) begin
       read_ready            <=  0;
     end
+
     //keep the last peice of data around
     if (read_fifo_select[0]) begin
-      last_read_data          <=  fifo0_read_data; 
+      waiting[0]              <=  0;
+      last_read_data          <=  fifo0_read_data;
     end
     if (read_fifo_select[1]) begin
-      last_read_data           <=  fifo1_read_data; 
+      waiting[1]              <=  0;
+      last_read_data          <=  fifo1_read_data;
     end
 
     //reset select
+//XXX: This will not allow single reads
+//XXX: Need a way to allow single reads
     //reset FIFO 0 select
-    if (read_fifo_select[0] && fifo0_empty) begin
+    if (read_fifo_select[0] && fifo0_empty && !read_ready) begin
+    //if (read_fifo_select[0] && !single_read[0] && fifo0_empty) begin
       read_fifo_select[0]     <=  0;
-      read_ready              <=  0;
       //gives the other buffer priority (this is useful for very slow read clocks)
       //fifo0 can be re-filled before the block can select the other FIFO
       read_pre_select[1]      <=  1;
     end
 
-    //reset FIFO 1 select 
-    if (read_fifo_select[1] && fifo1_empty) begin
+    //reset FIFO 1 select
+    if (read_fifo_select[1] && fifo1_empty && !read_ready) begin
+    //if (read_fifo_select[1] && !single_read[1] && fifo1_empty) begin
       read_fifo_select[1]     <=  0;
-      read_ready              <=  0;
       //gives the other buffer priroity (this is useful for very slow read clocks)
       read_pre_select[0]      <=  1;
     end
-
   end
 end
 endmodule
