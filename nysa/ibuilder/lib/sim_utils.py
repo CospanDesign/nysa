a57ba83e082dab2a758918fedf6daea124173a68
cospan <cospan@gmail.com>
2013-09-16 12:08:41 -0400

    ADD: sim_utils and test bench
    This will help when generating a testbench for entire board simulations

---
+++ nysa/ibuilder/lib/sim_utils.py
@@ -0,0 +1,201 @@
+# Copyright (c) 2013 Dave McCoy (dave.mccoy@cospandesign.com)
+#
+# This file is part of Nysa.
+#
+#       (http://wiki.cospandesign.com/index.php?title=Nysa.org)
+#
+# Nysa is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# any later version.
+#
+# Nysa is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Nysa; If not, see <http://www.gnu.org/licenses/>.
+
+# -*- coding: utf-8 -*-
+
+"""sim_utils
+
+Utilities used to generate simulation interfaces
+
+"""
+
+"""
+Changes:
+9/16/2013
+    -Initial Commit
+"""
+
+__author__ = 'dave.mccoy@cospandesign.com (Dave McCoy)'
+
+import os
+import sys
+import json
+
+#Project Modules
+import utils
+import verilog_utils as vutils
+import ibuilder_error
+
+class SimUtilsError(ibuilder_error.IBuilderError):
+    pass
+
+
+SIM_EXTENSION = ".json"
+sim_path = os.path.abspath(os.path.join(os.path.dirname(__file__),
+                                        os.pardir,
+                                        "sim"))
+
+def get_sim_module_dict(module_name, user_paths = [], debug = False):
+    """
+    Get the sim module tags associated with the specified name
+
+    takes in a name of a module to be tested, E.G. 'wb_sdram' and searches
+    for a configuration file that can be used to setup a test for this module
+
+    Args:
+        module_name (string): name of the module that is to be tested.
+        user_paths (list of strings): paths to prepend to the search directory
+            when looking for modules
+
+    Returns:
+        (dict): A dictionary of module tags that will be used to build a
+            simulation interface
+
+    Raises:
+        SimUtilsError:
+                -Sim File was not found
+                -A user path was not valit
+        TypeError:
+                -Pass JSON Errors up
+        ValueError
+                -Pass JSON Errors up
+    """
+    paths = user_paths
+    paths.append(sim_path)
+    sim_filename = "%s.%s" % (module_name, SIM_EXTENSION)
+    #Setinal value to catch if the path to the sim module dict doesn't exist
+    sim_filepath = None
+    found_path = False
+    for path in paths:
+        if found_path:
+            #Fond the first occurance of the file we're looking for
+            break
+        if not os.path.exists(path):
+            raise SimUtilsError("sim_utils: path: %s is not a valid" % path)
+        if not os.path.isdir(path):
+            raise SimUtilsError("sim_utils: path %s is not a directory" % path)
+
+        for root, dirs, names in os.walk(path):
+            for sim_filename in names:
+                sim_filepath = os.path.abspath(os.path.join(root, sim_filename))
+                #Drop out of the outer loop too (double break!)
+                found_path = True
+                break
+
+    #Check if we captures something
+    if sim_filepath is None:
+        raise SimUtilsError("sim_utils: did not find %s" % sim_filename,
+                            "searched: %s" % str(paths))
+    sim_dict = None
+    try:
+        fp = open(sim_filepath, "r")
+        if debug: print "Open: %s" % sim_filepath
+        sim_dict = json.loads(fp.read())
+        fp.close()
+    except IOError, err:
+        raise SimUtilsError("sim_utils: Failed to open file %s: Error: %s" % 
+                (sim_filepath, str(err)))
+
+
+    return sim_dict
+    
+
+def generate_sim_module_buf(invert_reset,
+                            sim_module_tags,
+                            debug = False):
+    """
+    Generates a sim module buffer that is suitable to be put in the testbench
+
+    Args:
+        invert_reset (boolean):
+            True: reset is inverted
+            False: reset is not inverted
+        sim_module_tags (dict): A dictionary of simulation tags used to
+            generate the buffer
+    
+    Returns:
+        (string): buffer that will instantiate the simulation module
+
+    Raises:
+        Nothing
+
+    """
+    #Look through the sim_module_tags for the inout ports
+    nysa_base = utils.get_nysa_base()
+    #search_path = os.path.join(nysa_base,
+    #                           "cbuilder",
+    #                           "verilog")
+    #filename = utils.find_module_filename(sim_module_tags["name"], [search_path])
+    filename = utils.find_module_filename(sim_module_tags["name"])
+    if debug: print "Module filename: %s" % filename
+    filepath = utils.find_rtl_file_location(filename)
+    if debug: print "Module filepath: %s" % filepath
+    module_tags = vutils.get_module_tags(filepath)
+    #if debug: print "Module Tags: %s" % str(module_tags)
+    #if debug: print "Ports: %s" % str(sim_module_tags["bind"].keys())
+
+    bind_list = []
+
+    for port in sim_module_tags["bind"].keys():
+        #if debug: print "port: %s" % port
+        #if debug: print "partition: %s" % str(port.partition("[")[0])
+        if len(port.partition("[")[0]) > 0:
+            name = port.partition("[")[0]
+            if name not in bind_list:
+                bind_list.append(name)
+        else:
+            if port not in bind_list:
+                bind_list.append(port)
+
+    #if debug:
+    #    print "bind list:"
+    #    for bind in bind_list:
+    #        print "\t%s" % bind
+    #    print ""
+        
+    #Need to fake out the verilog_utils port interface to thinking we are just a
+    #   normal slave module with port declaration for the entire module
+    inout_ports = {}
+    inout_ports["bind"] = {}
+    if "inout" in module_tags["ports"]:
+        for inout_port in module_tags["ports"]["inout"]:
+            #if debug: print "inout_port: %s" % str(inout_port)
+            if inout_port not in sim_module_tags["bind"]:
+                continue
+
+            #inout_ports["bind"]["inout"] = module_tags["ports"]["inout"]
+            inout_ports["bind"][inout_port] = {}
+
+            inout_ports["bind"][inout_port]["loc"] = sim_module_tags["bind"][inout_port]
+
+    if debug:
+        print "inout ports"
+        for inout in inout_ports["bind"]:
+            print "\t%s:%s" % (inout, inout_ports["bind"][inout])
+
+    buf = vutils.generate_module_port_signals(invert_reset,
+                                              "",
+                                              inout_ports,
+                                              module_tags)
+
+    return buf
+                                                
+        
+
+
---
+++ test/test_sim_utils.py
@@ -0,0 +1,105 @@
+#! /usr/bin/python
+
+import unittest
+import sys
+import os
+
+sys.path.append(os.path.join(os.path.dirname(__file__), os.pardir, 'nysa'))
+
+from ibuilder.lib import sim_utils as sutils
+from ibuilder.lib import utils
+from ibuilder.lib.ibuilder_error import IBuilderError
+
+test_dict = {
+  "name":"mt48lc4m16",
+  "aux_files":[
+    "mt48lc4m16.ftmv",
+    "mt48lc4m16.mem"
+  ],
+  "bind":{
+    "A11"     : "o_sdram_addr[11]",
+    "A10"     : "o_sdram_addr[10]",
+    "A9"      : "o_sdram_addr[9]",
+    "A8"      : "o_sdram_addr[8]",
+    "A7"      : "o_sdram_addr[7]",
+    "A6"      : "o_sdram_addr[6]",
+    "A5"      : "o_sdram_addr[5]",
+    "A4"      : "o_sdram_addr[4]",
+    "A3"      : "o_sdram_addr[3]",
+    "A2"      : "o_sdram_addr[2]",
+    "A1"      : "o_sdram_addr[1]",
+    "A0"      : "o_sdram_addr[0]",
+
+    "DQ15"    : "io_sdram_data[15]",
+    "DQ14"    : "io_sdram_data[14]",
+    "DQ13"    : "io_sdram_data[13]",
+    "DQ12"    : "io_sdram_data[12]",
+    "DQ11"    : "io_sdram_data[11]",
+    "DQ10"    : "io_sdram_data[10]",
+    "DQ9"     : "io_sdram_data[9]",
+    "DQ8"     : "io_sdram_data[8]",
+    "DQ7"     : "io_sdram_data[7]",
+    "DQ6"     : "io_sdram_data[6]",
+    "DQ5"     : "io_sdram_data[5]",
+    "DQ4"     : "io_sdram_data[4]",
+    "DQ3"     : "io_sdram_data[3]",
+    "DQ2"     : "io_sdram_data[2]",
+    "DQ1"     : "io_sdram_data[1]",
+    "DQ0"     : "io_sdram_data[0]",
+
+    "CLK"     : "o_sdram_clk",
+    "CKE"     : "o_sdram_cke",
+    "CASeg"   : "o_sdram_cs_n",
+    "RASNeg"  : "o_sdram_ras",
+    "CASNeg"  : "o_sdram_cas",
+    "WENeg"   : "o_sdram_we",
+    "BA1"     : "o_sdram_bank[1]",
+    "BA0"     : "o_sdram_bank[0]",
+    "DQMH"    : "o_sdram_data_mask[1]",
+    "DQML"     : "o_sdram_data_mask[0]"
+  }
+}
+
+class Test (unittest.TestCase):
+    """Unit test for verilog_utils"""
+
+    def setUp(self):
+        base = os.path.join( os.path.dirname(__file__),
+                             os.pardir)
+        self.nysa_base = os.path.abspath(base)
+        self.dbg = False
+
+    def test_get_sim_module_dict_pass(self):
+        """
+        Test the function to get the dictionary of the interface to the test
+        module of the specified module (Sorry this is confusing)
+        """
+        module_name = "wb_sdram"
+        user_paths = []
+        prev_dbg = self.dbg
+        #self.dbg = True
+        sdict = sutils.get_sim_module_dict(module_name,
+                                           user_paths,
+                                           debug = self.dbg)
+
+        self.dbg = prev_dbg
+        self.assertIsNotNone(sdict)
+
+
+
+
+    def test_generate_sim_module_buf(self):
+        """Test generate sim moulde buf"""
+
+        module_name = "wb_sdram"
+        user_paths = []
+        prev_dbg = self.dbg
+        self.dbg = True
+        buf = "%s sdram_sim " % test_dict["name"]
+        buf += sutils.generate_sim_module_buf(False,
+                                              test_dict,
+                                              debug = self.dbg)
+        if self.dbg:
+            print "sim module buffer:\n%s" % buf
+        self.dbg = prev_dbg
+
