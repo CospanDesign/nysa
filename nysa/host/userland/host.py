fbb3b5d424a5cdd1837370d4c9e2ccf5dbf7c8a8
cospan <cospan@gmail.com>
2013-09-16 03:13:51 -0400

    SMALL EDIT: Changed errro in license

--- nysa/host/userland/host.py
+++ nysa/host/userland/host.py
@@ -1,6 +1,6 @@
 # Copyright (c) 2013 Dave McCoy (dave.mccoy@cospandesign.com)
 
-# This file is part of Nysa (http://ninja-ide.org).
+# This file is part of Nysa (wiki.cospandesign.com/index.php?title=Nysa).
 #
 # Nysa is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
