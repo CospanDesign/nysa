96cbff90ff854ccb413f7ae7a20f5f39a3f7be33
cospan <cospan@gmail.com>
2013-09-18 14:50:09 -0400

    SMALL CHANGE: Change license from MIT to GPL V3

--- nysa/ibuilder/lib/module_processor.py
+++ nysa/ibuilder/lib/module_processor.py
@@ -1,23 +1,23 @@
-#Distributed under the MIT licesnse.
-#Copyright (c) 2011 Dave McCoy (dave.mccoy@cospandesign.com)
+# Copyright (c) 2011 Dave McCoy (dave.mccoy@cospandesign.com)
+#
+# This file is part of Nysa.
+#
+#       (http://wiki.cospandesign.com/index.php?title=Nysa.org)
+#
+# Nysa is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# any later version.
+#
+# Nysa is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Nysa; If not, see <http://www.gnu.org/licenses/>.
 
-#Permission is hereby granted, free of charge, to any person obtaining a copy of
-#this software and associated documentation files (the "Software"), to deal in
-#the Software without restriction, including without limitation the rights to
-#use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
-#of the Software, and to permit persons to whom the Software is furnished to do
-#so, subject to the following conditions:
-#
-#The above copyright notice and this permission notice shall be included in all
-#copies or substantial portions of the Software.
-#
-#THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-#IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-#FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-#AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-#LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-#OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-#SOFTWARE.
+# -*- coding: utf-8 -*-
 
 """Module Factory
 
@@ -29,11 +29,13 @@ of the two
 __author__ = 'dave.mccoy@cospandesign.com (Dave McCoy)'
 
 """Changes:
-  06/11/2012
+06/11/2012
     -Added Documentation and licsense
     -Moved two functions from sapfile to utils
       is_module_in_file
       find_module_filename
+09/18/2013
+    -Changed license to GPL V3
 """
 
 
