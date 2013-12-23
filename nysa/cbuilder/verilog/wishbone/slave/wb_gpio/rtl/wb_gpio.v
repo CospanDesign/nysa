4194e7afde654e2823f6ce2f889c9b1fbb9c773f
cospan <cospan@gmail.com>
2013-12-22 21:39:14 -0500

    Added a submodule ID to the GPIO

--- nysa/cbuilder/verilog/wishbone/slave/wb_gpio/rtl/wb_gpio.v
+++ nysa/cbuilder/verilog/wishbone/slave/wb_gpio/rtl/wb_gpio.v
@@ -48,6 +48,8 @@ SOFTWARE.
 	identification of your device 0 - 65536
 	DRT_ID:  1
 
+  DRT_SUB_ID: 1
+
 	flags (read drt.txt in the slave/device_rom_table directory 1 means
 	a standard device
 	DRT_FLAGS:  1
