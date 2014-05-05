d83c57aea086d7441d4acafba4f9462adb4543b2
cospan <cospan@gmail.com>
2014-05-04 21:59:36 -0400

    Debugged camera_utils.py

--- nysa/host/userland/python/protocol_utils/sf_camera/camera_utils.py
+++ nysa/host/userland/python/protocol_utils/sf_camera/camera_utils.py
@@ -48,9 +48,10 @@ class IMAGE_SIZES(object):
     SUB_QCIF = 8
     ZSUB_QCIF = 9
 
-class CameraThreadWorker(QObject):
+class CameraThreadWorker(QThread):
 
     def __init__(self, camera, width, height, mutex, img_format, actions, debug = False):
+        super (CameraThreadWorker, self).__init__()
         self.debug = debug
         self.camera = camera
         self.mutex = mutex
@@ -73,38 +74,75 @@ class CameraThreadWorker(QObject):
         self.term_flag = True
 
     def run(self):
-        while self.term_flag:
+        while not self.term_flag:
             #Try and lock, every 100ms refresh so if the user wants to exit we are not stuck
             if self.mutex.tryLock(0.100):
-                image = self.camera.get_raw_image()
-                qimage = QtGui.QImage(image, self.width, self.height, self.img_format)
+                image = self.camera.read_raw_image()
+                qimage = QImage(image, self.width, self.height, self.img_format)
+                #qimage.save("image.png", "png")
                 self.actions.sf_camera_read_ready.emit(qimage)
                 self.mutex.unlock()
 
 class CameraUtils(QObject):
-    def __init__(self, camera):
+    def __init__(self, camera, actions, debug):
+        super(CameraUtils, self).__init__()
         self.img_format = QImage.Format_RGB16
         self.mutex = QMutex()
         self.camera_thread_worker = None
         self.height = 0
         self.width = 0
+        self.camera = camera
+        self.actions = actions
+        self.debug = debug
 
     def setup_camera(self):
+        self.camera.set_control(0x00)
         self.camera.reset_camera()
-        self.set_rgb_mode()
+        self.camera.set_rgb_mode()
         self.camera.reset_counts()
         time.sleep(.5)
-        row_count = self.camera_row_count()
+        row_count = self.camera.read_row_count()
+        print "row count: %d" % row_count
         pixel_count = self.camera.read_pixel_count()
+        print "pixel count: %d" % (pixel_count / 2)
         self.height = row_count
         self.width = pixel_count / 2 #Asuming RGB565: then 2 bytes per pixel
-        self.camera_thread_worker = CameraThreadWorker(self.camera,
-                                                       self.width,
-                                                       self.height,
-                                                       self.mutex,
-                                                       self.img_format,
-                                                       self.actions,
-                                                       self.debug)
+        self.camera.enable_camera(True)
+        self.camera.get_status()
+
+    def run(self):
+        if self.camera_thread_worker is None:
+            self.camera_thread_worker = CameraThreadWorker(self.camera,
+                                                           self.width,
+                                                           self.height,
+                                                           self.mutex,
+                                                           self.img_format,
+                                                           self.actions,
+                                                           self.debug)
+        if self.camera_thread_worker.isRunning():
+            #Already Running just exit
+            return
+
+        else:
+            self.camera_thread_worker.start()
+
+    def stop(self):
+        if self.camera_thread_worker is None:
+            return
+        if self.camera_thread_worker.isRunning():
+            self.camera_thread_worker.end_reading()
+            self.camera_thread_worker.wait()
+
+        self.camera_thread_worker = None
+
+    def reset(self):
+        if self.camera_thread_worker is not None:
+            if self.camera_thread_worker.isRunning():
+                self.camera_thread_worker.end_reading()
+                self.camera_thread_worker.wait()
+            self.camera_thread_worker = None
+
+        self.setup_camera()
 
     def set_black_and_white(self):
         pass
