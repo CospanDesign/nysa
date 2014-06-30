1128b60977cc621d7c23d1309433c7dc852c208b
cospan <dave.mccoy@cospandesign.com>
2014-06-29 23:41:12 -0400

    Updated app template

--- nysa/host/userland/python/apps/app_template/controller.py
+++ nysa/host/userland/python/apps/app_template/controller.py
@@ -35,14 +35,27 @@ sys.path.append(os.path.join(os.path.dirname(__file__),
                              os.pardir,
                              os.pardir))
 
+#from driver import ???
+
 from apps.common.nysa_base_controller import NysaBaseController
 import apps
 
 from view.view import View
-from model.model import AppModel
+
+#Platform Scanner
+sys.path.append(os.path.join(os.path.dirname(__file__),
+                             os.pardir,
+                             os.pardir,
+                             "common"))
+
+
+from platform_scanner import PlatformScanner
+import status
 
 #Module Defines
 n = str(os.path.split(__file__)[1])
+
+
 
 DESCRIPTION = "\n" \
 "\n"\
@@ -60,7 +73,7 @@ class Controller(NysaBaseController):
 
     def __init__(self):
         super (Controller, self).__init__()
-        self.m = AppModel()
+        pass
 
     @staticmethod
     def get_name():
@@ -71,13 +84,21 @@ class Controller(NysaBaseController):
         self.v = View(self.status, self.actions)
         self.v.setup_simple_text_output_view()
 
-    def start_standalone_app(self):
+    def start_standalone_app(self, platform, device_index, debug = False):
         app = QApplication (sys.argv)
-        self._initialize()
+        self.status = status.ClStatus()
+        if debug:
+            self.status.set_level(status.StatusLevel.VERBOSE)
+        else:
+            self.status.set_level(status.StatusLevel.INFO)
+        self.status.Verbose(self, "Starting Standalone Application")
+        self._initialize(platform, device_index)
         sys.exit(app.exec_())
 
-    def start_tab_view(self, platform):
-        self._initialize()
+    def start_tab_view(self, platform, device_index):
+        self.status = status.Status()
+        self.status.Verbose(self, "Starting Template Application")
+        self._initialize(platform, device_index)
 
     def get_view(self):
         return self.v
@@ -118,6 +139,8 @@ class Controller(NysaBaseController):
 
 def main(argv):
     #Parse out the commandline arguments
+    s = status.ClStatus()
+    s.set_level(status.StatusLevel.INFO)
     parser = argparse.ArgumentParser(
             formatter_class = argparse.RawDescriptionHelpFormatter,
             description = DESCRIPTION,
@@ -128,15 +151,73 @@ def main(argv):
     parser.add_argument("-d", "--debug",
                         action = "store_true",
                         help = "Enable Debug Messages")
-
+    parser.add_argument("-l", "--list",
+                        action = "store_true",
+                        help = "List the available devices from a platform scan")
+    parser.add_argument("platform",
+                        type = str,
+                        nargs='?',
+                        default=["first"],
+                        help="Specify the platform to use")
+ 
     args = parser.parse_args()
+    plat = None
 
     if args.debug:
-        print ("Debug Enable")
+        s.set_level(status.StatusLevel.VERBOSE)
+        s.Debug(None, "Debug Enabled")
         debug = True
 
+    pscanner = PlatformScanner()
+    ps = pscanner.get_platforms()
+    dev_index = None
+    for p in ps:
+        s.Verbose(None, p)
+        for psi in ps[p]:
+            if plat is None:
+                s.Verbose(None, "Found a platform: %s" % p)
+                n = ps[p][psi]
+                n.read_drt()
+                dev_index = None
+                #dev_index = n.find_device(I2C.get_core_id())
+                if dev_index is not None:
+                    print "Dev Index: %d" % dev_index
+                    plat = [p, psi, ps[p][psi]]
+                    break
+                continue
+            if p == args.platform and plat[0] != args.platform:
+                #Found a match for a platfom to use
+                plat = [p, psi, ps[p][psi]]
+                continue
+            if p == psi:
+                #Found a match for a name!
+                #See if we can find a device
+
+                dev_index = None
+                #dev_index = n.find_device(I2C.get_core_id())
+                print "Dev Index: %d" % dev_index
+                if dev_index is not None:
+                    plat = [p, psi, ps[p][psi]]
+                    break
+
+            s.Verbose(None, "\t%s" % psi)
+
+    if args.list:
+        s.Verbose(None, "Listed all platforms, exiting")
+        sys.exit(0)
+
+    if plat is not None:
+        s.Important(None, "Using: %s" % plat)
+    else:
+        s.Fatal(None, "Didn't find a platform to use!")
+
+
     c = Controller()
-    c.start_standalone_app()
+    if dev_index is None:
+        sys.exit("Failed to find an Device")
+
+    c.start_standalone_app(plat, dev_index, debug)
 
 if __name__ == "__main__":
     main(sys.argv)
+
