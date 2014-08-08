3faa4710d8b0a449ea327aaa67caa70c85d1477f
cospan <dave.mccoy@cospandesign.com>
2014-08-08 16:33:39 -0400

    Added a common directory

---
+++ nysa/common/status.py
@@ -0,0 +1,171 @@
+#! /usr/bin/python
+
+# Copyright (c) 2014 Dave McCoy (dave.mccoy@cospandesign.com)
+
+# This file is part of Nysa (wiki.cospandesign.com/index.php?title=Nysa).
+#
+# Nysa is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# any later version.
+#
+# Nysa is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Nysa; If not, see <http://www.gnu.org/licenses/>.
+
+import inspect
+
+def enum(*sequential, **named):
+  enums = dict(zip(sequential, range(len(sequential))), **named)
+  return type('Enum', (), enums)
+
+
+white = '\033[0m'
+gray = '\033[90m'
+red = '\033[91m'
+green = '\033[92m'
+yellow = '\033[93m'
+blue = '\033[94m'
+purple = '\033[95m'
+cyan = '\033[96m'
+
+StatusLevel = enum ('FATAL', 'ERROR', 'WARNING', 'INFO', 'IMPORTANT', 'DEBUG', 'VERBOSE')
+
+_status_instance = None
+
+def Status(*args, **kw):
+    global _status_instance
+    if _status_instance is None:
+        _status_instance = _Status(*args, **kw)
+    return _status_instance
+
+
+class _Status(object):
+    @staticmethod
+    def is_command_line():
+        return True
+
+    def __init__(self):
+        super(_Status, self).__init__()
+        #print "Staring CL Status"
+        self.level = StatusLevel.VERBOSE
+
+
+    def Verbose (self, c, text):
+        if self.CheckLevel(StatusLevel.VERBOSE):
+            self.status_output("Verbose", c, text, color = cyan)
+
+    def Debug (self, c, text):
+        if self.CheckLevel(StatusLevel.DEBUG):
+            self.status_output("Debug", c, text, color = green)
+
+    def Info (self, c, text):
+        if self.CheckLevel(StatusLevel.INFO):
+            self.status_output("Info", c, text, color = white)
+
+    def Important (self, c, text):
+        if self.CheckLevel(StatusLevel.IMPORTANT):
+            self.status_output("Important", c, text, color = blue)
+
+    def Warning (self, c, text):
+        if self.CheckLevel(StatusLevel.WARNING):
+            self.status_output("Warning", c, text, color = blue)
+
+    def Error (self, c, text):
+        if self.CheckLevel(StatusLevel.ERROR):
+            self.status_output("Error", c, text, color=red)
+
+    def Fatal (self, c, text):
+        if self.CheckLevel(StatusLevel.FATAL):
+            self.status_output("Fatal", c, text, color=red)
+
+    def Print (self, text):
+        self.status_output("", None, text)
+
+    def PrintLine(self, text):
+        self.status_output("", None, text)
+
+    def status_output(self, level, c, text, color=white):
+        if c is not None:
+            f = str(inspect.stack()[2][3])
+            #print "inspace: %s" % str(inspect.stack()[2])
+            d = "%s:%s " % (c.__class__.__name__, f)
+            text=d + text
+            print "%s%s: %s%s" % (color, level, text, white)
+        else:
+            #print "inspace: %s" % str(inspect.stack()[2])
+            print "%s%s: %s%s" % (color, level, text, white)
+
+    def set_level(self, level):
+        self.level = level
+
+    def GetLevel(self):
+        return self.level
+
+    def CheckLevel(self, requestLevel):
+        if requestLevel == StatusLevel.FATAL:
+            return True
+        elif requestLevel is StatusLevel.VERBOSE:
+            if  self.level == StatusLevel.VERBOSE:
+                return True
+        elif requestLevel is StatusLevel.DEBUG:
+            if  self.level == StatusLevel.VERBOSE or \
+                self.level == StatusLevel.DEBUG:
+                return True
+        elif requestLevel is StatusLevel.INFO:
+            if self.level == StatusLevel.VERBOSE or  \
+                self.level == StatusLevel.DEBUG or   \
+                self.level == StatusLevel.INFO:
+                return True
+        elif requestLevel is StatusLevel.IMPORTANT:
+            if self.level == StatusLevel.VERBOSE or  \
+                self.level == StatusLevel.DEBUG or   \
+                self.level == StatusLevel.INFO or    \
+                self.level == StatusLevel.IMPORTANT:
+                return True
+        elif requestLevel is StatusLevel.WARNING:
+            if self.level == StatusLevel.VERBOSE or  \
+                self.level == StatusLevel.DEBUG or   \
+                self.level == StatusLevel.INFO  or   \
+                self.level == StatusLevel.IMPORTANT or \
+                self.level == StatusLevel.WARNING:
+                return True
+        elif requestLevel is StatusLevel.ERROR:
+            if self.level == StatusLevel.VERBOSE or  \
+                self.level == StatusLevel.DEBUG or   \
+                self.level == StatusLevel.INFO  or   \
+                self.level == StatusLevel.IMPORTANT or \
+                self.level == StatusLevel.WARNING or \
+                self.level == StatusLevel.ERROR:
+                return True
+       
+        return False
+
+    def cl_status(level = 2, text = ""):
+        #if not text.endswith("\n"):
+        #    text = text + "\n"
+
+        if level == 0:
+            print "%sVerbose: %s%s" % (cyan, text, white)
+        elif level == 1:
+            print "%sDebug: %s%s" % (green, text, white)
+        elif level == 2:
+            print "%sInfo: %s%s" % (white, text, white)
+        elif level == 3:
+            print "%sImportant: %s%s" % (blue, text, white)
+        elif level == 4:
+            print "%sWarning: %s%s" % (yellow, text, white)
+        elif level == 5:
+            print "%sError: %s%s" % (red, text, white)
+        elif level == 6:
+            print "%sCritical: %s%s" % (red, text, white)
+        else:
+            print "Unknown Level (%d) Text: %s" % (level, text)
+
+
+
+
