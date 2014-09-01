6c92163fecb6a65157ceb7b215a3b3db5f977c74
cospan <dave.mccoy@cospandesign.com>
2014-09-01 19:42:55 -0400

    Small asthetic changes to the list board

--- nysa/tools/list_boards.py
+++ nysa/tools/list_boards.py
@@ -55,7 +55,7 @@ def list_boards(args, status):
         p = platform_class_dict[platform](s)
         dev_dict = p.scan()
         if len(dev_dict) > 0:
-            print "Found %d %s" % (len(dev_dict), platform)
+            print "Found %d board(s)" % (len(dev_dict), platform)
         else:
             print "No boards found"
     #platforms = pc.get_platforms()
