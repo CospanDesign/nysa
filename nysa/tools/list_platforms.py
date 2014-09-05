6762cb9ae3e08dd63f62e9bc3fbe5888d8bdbd7c
cospan <dave.mccoy@cospandesign.com>
2014-09-04 20:56:02 -0400

    Added new script to view a list of local platforms

---
+++ nysa/tools/list_platforms.py
@@ -0,0 +1,71 @@
+#Distributed under the MIT licesnse.
+#Copyright (c) 2014 Dave McCoy (dave.mccoy@cospandesign.com)
+
+#Permission is hereby granted, free of charge, to any person obtaining a copy of
+#this software and associated documentation files (the "Software"), to deal in
+#the Software without restriction, including without limitation the rights to
+#use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+#of the Software, and to permit persons to whom the Software is furnished to do
+#so, subject to the following conditions:
+#
+#The above copyright notice and this permission notice shall be included in all
+#copies or substantial portions of the Software.
+#
+#THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+#IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+#FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+#AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+#LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+#OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+#SOFTWARE.
+
+import sys
+import os
+import argparse
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir)))
+
+from host.platform_scanner import PlatformScanner
+from host.platform_scanner import PlatformScannerException
+
+SCRIPT_NAME = os.path.basename(__file__)
+
+__author__ = "dave.mccoy@cospandesign.com (Dave McCoy)"
+
+DESCRIPTION = "Display a list of the currently installed platforms"
+
+EPILOG = "\n"
+
+white = '\033[0m'
+gray = '\033[90m'
+red   = '\033[91m'
+green = '\033[92m'
+yellow = '\033[93m'
+blue = '\033[94m'
+purple = '\033[95m'
+cyan = '\033[96m'
+
+if os.name == "nt":
+    white  = ''
+    gray   = ''
+    red    = ''
+    green  = ''
+    yellow = ''
+    blue   = ''
+    purple = ''
+    cyan   = ''
+
+def setup_parser(parser):
+    parser.description = DESCRIPTION
+    return parser
+
+def list_platforms(args, status):
+    s = status
+    #print "args.name: %s" % args.name
+    ps = PlatformScanner(s)
+    platforms = ps.get_platforms()
+    print "%sPlatforms:%s" % (purple, white)
+    for platform in platforms:
+        print "\t%s%s%s" % (blue, platform, white)
+
+
