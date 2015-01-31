a3adeea6660c4d7bbe3582b1db46e7e622099624
cospan <cospan@gmail.com>
2015-01-31 18:40:01 -0500

    Updated tests for ibuilder

--- test/unit/test_ibuilder_main.py
+++ test/unit/test_ibuilder_main.py
@@ -15,30 +15,22 @@ TEST_CONFIG_FILENAME = os.path.abspath( os.path.join(os.path.dirname(__file__),
                                         "fake",
                                         "test_config_file.json"))
 
-
-
 class Test (unittest.TestCase):
-  """Unit test for ibuilder"""
+    """Unit test for ibuilder"""
 
-  def setUp(self):
-    base = os.path.join( os.path.dirname(__file__),
-                         os.pardir)
-    self.nysa_base = os.path.abspath(base)
-    self.out_dir = os.path.join(os.path.expanduser("~"), "sandbox", "temp")
-    self.dbg = False
+    def setUp(self):
+        self.out_dir = os.path.join(os.path.expanduser("~"), "sandbox", "temp")
 
-  def test_create_project_config_file(self):
-    """Test the create JSON string function"""
-    result = ibuilder.create_project_config_file(
-                filename = "output_project_config_file.json",
-                bus="wishbone",
-                interface="uart_io_handler.v",
-                base_dir=self.out_dir)
-    self.assertEqual(1, 1)
+    def test_get_project_tags(self):
+        test = ibuilder.get_project_tags(TEST_CONFIG_FILENAME)
+        self.assertEqual(test["PROJECT_NAME"], "example_project")
 
-  def test_generate_project(self):
-    result = ibuilder.generate_project(TEST_CONFIG_FILENAME)
-    self.assertEqual(result, True)
+    def test_get_output_dir(self):
+        test = ibuilder.get_output_dir(TEST_CONFIG_FILENAME)
+        output = os.path.join(os.path.expanduser("~"), "projects", "ibuilder_project")
+        self.assertEqual(test, output)
 
-if __name__ == "__main__":
-  unittest.main()
+    def test_generate_project(self):
+        result = ibuilder.generate_project(TEST_CONFIG_FILENAME)
+        self.assertEqual(result, True)
+
