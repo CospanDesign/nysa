34dbb719dde4e03951ab29e1f42fc578fdaa00e3
cospan <cospan@gmail.com>
2015-02-08 22:30:23 -0500

    Added memory and sdb to the dirvers

---
+++ nysa/host/driver/sdb.py
@@ -0,0 +1,44 @@
+#Distributed under the MIT licesnse.
+#Copyright (c) 2011 Dave McCoy (dave.mccoy@cospandesign.com)
+
+#Permission is hereby granted, free of charge, to any person obtaining a copy of
+#this software and associated documentation files (the "Software"), to deal in
+#the Software without restriction, including without limitation the rights to
+#use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+#of the Software, and to permit persons to whom the Software is furnished to do
+#so, subject to the following conditions:
+#
+#The above copyright notice and this permission notice shall be included in all
+#copies or substantial portions of the Software.
+#
+#THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+#IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+#FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+#AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+#LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+#OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+#SOFTWARE.
+
+""" SDB
+
+Facilitates communication with any Memmory core independent of communication
+medium
+
+"""
+
+__author__ = 'dave.mccoy@cospandesign.com (Dave McCoy)'
+
+import driver
+
+class SDB(driver.Driver):
+
+    @staticmethod
+    def get_abi_class():
+        return 0
+
+    @staticmethod
+    def get_abi_major():
+        return driver.get_device_id_from_name("sdb")
+
+    def __init__(self, nysa, urn, debug = False):
+        super(SDB, self).__init__(nysa, urn, debug)
