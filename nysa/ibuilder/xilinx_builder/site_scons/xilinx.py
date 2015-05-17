afeeeb4b27db5e1d67b8cc5445717ae66250ed52
Dave <cospan@gmail.com>
2015-05-17 11:13:42 -0400

    Updated nysa with the newest xilinx tools

--- nysa/ibuilder/xilinx_builder/site_scons/xilinx.py
+++ nysa/ibuilder/xilinx_builder/site_scons/xilinx.py
@@ -1,30 +1,31 @@
-#Distributed under the MIT licesnse.
-#Copyright (c) 2013 Cospan Design (dave.mccoy@cospandesign.com)
+# Distributed under the MIT licesnse.
+# Copyright (c) 2013 Cospan Design (dave.mccoy@cospandesign.com)
 
-#Permission is hereby granted, free of charge, to any person obtaining a copy of
-#this software and associated documentation files (the "Software"), to deal in
-#the Software without restriction, including without limitation the rights to
-#use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
-#of the Software, and to permit persons to whom the Software is furnished to do
-#so, subject to the following conditions:
+# Permission is hereby granted, free of charge, to any person obtaining a copy of
+# this software and associated documentation files (the "Software"), to deal in
+# the Software without restriction, including without limitation the rights to
+# use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+# of the Software, and to permit persons to whom the Software is furnished to do
+# so, subject to the following conditions:
 #
-#The above copyright notice and this permission notice shall be included in all
-#copies or substantial portions of the Software.
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
 #
-#THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-#IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-#FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-#AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-#LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-#OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-#SOFTWARE.
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
 
 import os
 import shutil
 import string
+
 try:
     import getpass
-except:
+except ImportError:
     pass
 
 import utils
@@ -37,13 +38,16 @@ import trace_utils
 import bitgen_utils
 import coregen_utils
 
+
 class XilinxNotImplimented(Exception):
     """XilinxNotImplemented
 
-    Errors associated with not implementing stuff
-    """
+	Errors associated with not implementing stuff
+	"""
+
     def __init__(self, value):
         self.value = value
+
     def __str__(self):
         return repr(self.value)
 
@@ -52,32 +56,32 @@ def test_xst():
     print "Hello"
 
 
-def initialize_environment(env, xilinx_path = "", build_tool = "ISE", version_number = ""):
+def initialize_environment(env, xilinx_path="", build_tool="ISE", version_number=""):
     """
-    Initialized an environment to contain all the xilinx build tools
+	Initialized an environment to contain all the xilinx build tools
 
-    Args:
-        env (Scons.Environment): An environment to add all the xilinx tools too
-        xilinx_path (string): A user defined path to the xilinx base directory
-            (leave empty to use the default system location)
-        build_type (string): to use, valid build types are found with
-            utils.get_xilinx_tool_types
-            (leave empty to use "ISE")
-        version_number (string): specify a version number to use for one of the
-            tool chains: e.g.
-                build_tool = ISE -> version number = 13.2
-                build_tool = Vivado -> version number = 2013.1
-            (leave empty for the latest version)
+	Args:
+		env (Scons.Environment): An environment to add all the xilinx tools too
+		xilinx_path (string): A user defined path to the xilinx base directory
+			(leave empty to use the default system location)
+		build_type (string): to use, valid build types are found with
+			utils.get_xilinx_tool_types
+			(leave empty to use "ISE")
+		version_number (string): specify a version number to use for one of the
+			tool chains: e.g.
+				build_tool = ISE -> version number = 13.2
+				build_tool = Vivado -> version number = 2013.1
+			(leave empty for the latest version)
 
-    Returns:
-        SCons.Environment): with the xilinx tools added
+	Returns:
+		SCons.Environment): with the xilinx tools added
 
-    Raises:
-        Configuration Error
-    """
+	Raises:
+		Configuration Error
+	"""
     xpath = utils.find_xilinx_path(xilinx_path, build_tool, version_number)
 
-    #print "path to xilinx build tool: %s " % xpath
+    # print "path to xilinx build tool: %s " % xpath
     env['XIL_SCRIPT_LOC'] = xpath
     env['XILINX_DSP'] = xpath
     env['XILINX_PLANAHEAD'] = os.path.join(xpath, "PlanAhead")
@@ -85,29 +89,32 @@ def initialize_environment(env, xilinx_path = "", build_tool = "ISE", version_nu
 
     if "USER" not in os.environ:
         os.environ["USER"] = getpass.getuser()
-	if "XILINXD_LICENSE_FILE" in os.environ:
-		#print "Found License File Location: %s" % os.environ["XILINXD_LICENSE_FILE"]
-		env['ENV']['XILINXD_LICENSE_FILE'] = os.environ["XILINXD_LICENSE_FILE"]
-	if "LM_LICENSE_FILE" in os.environ:
-		env['ENV']['LM_LICENSE_FILE'] = os.environ["LM_LICENSE_FILE"]
+    if "XILINXD_LICENSE_FILE" in os.environ:
+        #print "Found License File Location: %s" % os.environ["XILINXD_LICENSE_FILE"]
+        env['ENV']['XILINXD_LICENSE_FILE'] = os.environ["XILINXD_LICENSE_FILE"]
+    if "LM_LICENSE_FILE" in os.environ:
+        env['ENV']['LM_LICENSE_FILE'] = os.environ["LM_LICENSE_FILE"]
     env['ENV']['USER'] = os.environ["USER"]
-    env['ENV']['HOME'] = os.environ["HOME"]
+    if os.name == "nt" and 'HOME' not in env['ENV'].keys():
+        env['ENV']['HOME'] = os.environ['USERPROFILE']
+    else:
+        env['ENV']['HOME'] = os.environ["HOME"]
 
     if 'LD_LIBRARY_PATH' not in env:
         env['LD_LIBRARY_PATH'] = ''
-    #print "xilinx path: %s" % xilinx_path
-    #print "build tool: %s" % build_tool
-    #print "version number: %s" % version_number
-    #print "xpath: %s" % xpath
-    #This is used for the license file
-	if os.name == "nt":
-		env["ENV"]["USERNAME"] = env['ENV']["USER"]
+    # print "xilinx path: %s" % xilinx_path
+    # print "build tool: %s" % build_tool
+    # print "version number: %s" % version_number
+    # print "xpath: %s" % xpath
+    # This is used for the license file
+    if os.name == "nt":
+        env["ENV"]["USERNAME"] = env['ENV']["USER"]
 
     if build_tool.lower() == "ise" or build_tool.lower() == "planahead":
         env.AppendENVPath("PATH", os.path.join(xpath, "PlanAhead", "bin"))
         env.AppendENVPath("PATH", os.path.join(xpath, "ISE", "sysgen", "util"))
         if utils.is_64_bit():
-            #64 bit machine
+            # 64 bit machine
             if os.name == "nt":
                 env.AppendENVPath("PATH", os.path.join(xpath, "common", "bin", "nt64"))
                 env.AppendENVPath("PATH", os.path.join(xpath, "ISE", "bin", "nt64"))
@@ -115,7 +122,7 @@ def initialize_environment(env, xilinx_path = "", build_tool = "ISE", version_nu
                 env['LD_LIBRARY_PATH'] = string.join([lib_path, env['LD_LIBRARY_PATH']], os.pathsep)
                 lib_path = os.path.join(xpath, "common", "lib", "nt64")
                 env['LD_LIBRARY_PATH'] = string.join([lib_path, env['LD_LIBRARY_PATH']], os.pathsep)
-                #print "LD_LIBRARY_PATH: %s" % str(env['LD_LIBRARY_PATH'])
+            # print "LD_LIBRARY_PATH: %s" % str(env['LD_LIBRARY_PATH'])
             else:
                 env.AppendENVPath("PATH", os.path.join(xpath, "common", "bin", "lin64"))
                 env.AppendENVPath("PATH", os.path.join(xpath, "ISE", "bin", "lin64"))
@@ -123,78 +130,87 @@ def initialize_environment(env, xilinx_path = "", build_tool = "ISE", version_nu
                 env['LD_LIBRARY_PATH'] = string.join([lib_path, env['LD_LIBRARY_PATH']], os.pathsep)
                 lib_path = os.path.join(xpath, "common", "lib", "lin64")
                 env['LD_LIBRARY_PATH'] = string.join([lib_path, env['LD_LIBRARY_PATH']], os.pathsep)
-                #print "LD_LIBRARY_PATH: %s" % str(env['LD_LIBRARY_PATH'])
+                # print "LD_LIBRARY_PATH: %s" % str(env['LD_LIBRARY_PATH'])
         else:
-            #32 bit machine
+            # 32 bit machine
             env.AppendENVPath("PATH", os.path.join(xpath, "common", "bin", "lin"))
             env.AppendENVPath("PATH", os.path.join(xpath, "ISE", "bin", "lin"))
             lib_path = os.path.join(xpath, "ISE", "lib", "lin")
             env['LD_LIBRARY_PATH'] = string.join([lib_path, env['LD_LIBRARY_PATH']], os.pathsep)
             lib_path = os.path.join(xpath, "common", "lib", "lin")
             env['LD_LIBRARY_PATH'] = string.join([lib_path, env['LD_LIBRARY_PATH']], os.pathsep)
-            #print "LD_LIBRARY_PATH: %s" % str(env['LD_LIBRARY_PATH'])
+            # print "LD_LIBRARY_PATH: %s" % str(env['LD_LIBRARY_PATH'])
     else:
 
         raise XilinxNotImplemented("Vivado is not implemented yet")
 
     return env
 
+
 def get_xst_targets(env):
     """
-    Returns a list of build target for the XST
-    """
+	Returns a list of build target for the XST
+	"""
     config = utils.read_config(env)
-    return xst_utils.get_ngc_filename(config, absolute = True)
+    return xst_utils.get_ngc_filename(config, absolute=True)
+
 
 def get_ngd_targets(env):
     config = utils.read_config(env)
-    return ngd_utils.get_ngd_filename(config, absolute = True)
+    return ngd_utils.get_ngd_filename(config, absolute=True)
+
 
 def get_map_targets(env):
     config = utils.read_config(env)
-    return map_utils.get_map_filename(config, absolute = True)
+    return map_utils.get_map_filename(config, absolute=True)
+
 
 def get_par_targets(env):
     config = utils.read_config(env)
-    return par_utils.get_par_filename(config, absolute = True)
+    return par_utils.get_par_filename(config, absolute=True)
+
 
 def get_trace_targets(env):
     config = utils.read_config(env)
-    return trace_utils.get_trace_filename(config, absolute = True)
+    return trace_utils.get_trace_filename(config, absolute=True)
+
 
 def get_bitgen_targets(env):
     config = utils.read_config(env)
-    return bitgen_utils.get_bitgen_filename(config, absolute = True)
+    return bitgen_utils.get_bitgen_filename(config, absolute=True)
+
 
 def get_coregen_targets(env):
     config = utils.read_config(env)
     return coregen_utils.get_target_files(config)
 
+
 def clean_cores(env):
     """
-    Cores take a terribly long time to build so unless the user obliterates
-    everything with a 'clean_build' don't remove the generated cores unless
-    the user explicitly asks for it
+	Cores take a terribly long time to build so unless the user obliterates
+	everything with a 'clean_build' don't remove the generated cores unless
+	the user explicitly asks for it
 
-    Args:
-        env (SCons Environment): Current environment
+	Args:
+		env (SCons Environment): Current environment
 
-    Returns:
-        Empty promises
+	Returns:
+		Empty promises
 
-    Raises:
-        Hopes and Dreams
-    """
+	Raises:
+		Hopes and Dreams
+	"""
     core_targets = get_coregen_targets(env)
     for core in core_targets:
         if os.path.exists(core):
             os.remove(core)
     return None
 
+
 def clean_build(env):
     config = utils.read_config(env)
     base_dir = utils.get_project_base()
-    build_dir = utils.get_build_directory(config, absolute = True)
+    build_dir = utils.get_build_directory(config, absolute=True)
     xmsgs_dir = os.path.join(base_dir, "_xmsgs")
     xlnx_auto = os.path.join(base_dir, "xlnx_auto_0_xdb")
     config_log = os.path.join(base_dir, "config.log")
@@ -206,9 +222,8 @@ def clean_build(env):
     par_report = "%s_par.xrpt" % config["top_module"]
     par_report = os.path.join(base_dir, par_report)
 
-    #Coregen
+    # Coregen
     coregen_log = os.path.join(base_dir, "coregen.log")
-
 
     print "Removing Directories/Files:"
 
@@ -239,6 +254,3 @@ def clean_build(env):
     if os.path.exists(coregen_log):
         print "\t%s" % coregen_log
         os.remove(coregen_log)
-
-
-
