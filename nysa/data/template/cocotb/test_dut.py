4977e53c68acbb7bc68300d3b2ed2caf69d1f560
cospan <cospan@gmail.com>
2015-08-05 16:27:12 -0400

    Added CocoTb stuff to the template directory
    Now users can use CocoTb when creating a slave core

---
+++ nysa/data/template/cocotb/test_dut.py
@@ -0,0 +1,55 @@
+# Simple tests for an adder module
+import os
+import sys
+import cocotb
+import logging
+from cocotb.result import TestFailure
+from nysa.host.sim.sim_host import NysaSim
+from cocotb.clock import Clock
+import time
+from array import array as Array
+
+SIM_CONFIG = "sim_config.json"
+
+
+CLK_PERIOD = 10
+
+MODULE_PATH = os.path.join(os.path.dirname(__file__), os.pardir, "rtl")
+MODULE_PATH = os.path.abspath(MODULE_PATH)
+
+
+def setup_sata(dut):
+    cocotb.fork(Clock(dut.sata_clk, SATA_CLK_PERIOD).start())
+
+@cocotb.coroutine
+def wait_ready(nysa, dut):
+
+    #while not dut.hd_ready.value.get_value():
+    #    yield(nysa.wait_clocks(1))
+
+    #yield(nysa.wait_clocks(100))
+    pass
+
+@cocotb.test(skip = False)
+def first_test(dut):
+    """
+    Description:
+        Very Basic Functionality
+            Startup Nysa
+
+    Test ID: 0
+
+    Expected Results:
+        Write to all registers
+    """
+
+
+    dut.test_id = 0
+    print "module path: %s" % MODULE_PATH
+    nysa = NysaSim(dut, SIM_CONFIG, CLK_PERIOD, user_paths = [MODULE_PATH])
+    yield(nysa.reset())
+    nysa.read_sdb()
+    nysa.pretty_print_sdb()
+    dut.log.info("DUT Opened!")
+    dut.log.info("Ready")
+
