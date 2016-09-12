9c466f94fbf1a80c9b7cb0ccc420ac478ee98e37
cospan <cospan@gmail.com>
2016-09-12 17:42:24 -0400

    Added methods to help print out hex strings

--- nysa/common/print_utils.py
+++ nysa/common/print_utils.py
@@ -3,4 +3,21 @@ def print_32bit_hex_array(hex_array):
     for i in range (0, len(hex_array), 4):
         print ("[8][{0:>4}] [32][{1:>4}]: {2:02X} {3:02X} {4:02X} {5:02X}".format(i, (i / 4), hex_array[i], hex_array[i + 1], hex_array[i + 2], hex_array[i + 3]))
 
+def print_8bit_hex_array(hex_array):
+    print list_to_hex_string(hex_array)
+
+def list_to_hex_string(a):
+    s = None
+    for i in a:
+        if s is None:
+            s = "["
+        else:
+            s += ", "
+
+        s += "0x%02X" % i
+
+    s += "]"
+    return s
+
+
 
